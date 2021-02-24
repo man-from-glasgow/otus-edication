d817f2a86213b80026db75d21c364837a0e9bb5c
rmaksimov <r.maksimov@semrush.com>
2021-02-24 19:06:52 +0300

    Fix streaming

--- homework_7_ml/src/main/scala/com/example/ml/IrisClassificationStreaming.scala
+++ homework_7_ml/src/main/scala/com/example/ml/IrisClassificationStreaming.scala
@@ -3,6 +3,7 @@ package com.example.ml
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.spark.SparkContext
 import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.{concat_ws, from_csv}
 import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
@@ -21,7 +22,11 @@ object IrisClassificationStreaming extends LazyLogging {
         Nil
     )
 
-    val irisData: DataFrame = spark.readStream
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("sepal_length", "sepal_width", "petal_length", "petal_width"))
+      .setOutputCol("features")
+
+    val irisDataDF: DataFrame = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:29092")
       .option("subscribe", "input")
@@ -34,16 +39,20 @@ object IrisClassificationStreaming extends LazyLogging {
       .withColumn("petal_width", $"struct".getField("petal_width"))
       .drop("value", "struct")
 
+    val irisData = assembler.transform(irisDataDF)
+
     val prediction = model.transform(irisData)
 
     val query = prediction
       .select(
-        concat_ws(",", $"sepal_length", $"sepal_width", $"petal_length", $"petal_width", $"predictionLabel").as("value")
+        $"predictedLabel".as("key"),
+        concat_ws(",",$"sepal_width", $"petal_length", $"petal_width", $"predictedLabel").as("value")
       )
       .writeStream
       .outputMode("append")
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:29092")
+      .option("checkpointLocation", "/tmp/checkpoint")
       .option("topic", "prediction")
       .start()
 
